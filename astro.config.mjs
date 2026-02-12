77a5e15e17067a5d04c48720f4cc3431afc89c0d
Matt Carroll <mc@qacafe.com>
2026-02-12 00:18:40 -0500

    remove svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import svelte from '@astrojs/svelte';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [svelte()]
-});
+export default defineConfig({});
